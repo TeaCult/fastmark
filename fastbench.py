3438c13df6c732f77927091f40787b56f9bcd47c
Gediz GÜRSU <gedizgursu@gmail.com>
2020-09-14 14:51:57 +0300

    Add files via upload

--- fastbench.py
+++ fastbench.py
@@ -9,107 +9,102 @@ TESTS=['build','texture','loop','shading','bump','effect2d','pulsar','desktop','
 
 key1="events per second:"
 
-print('\nA Fast Benchmark designed for impatients by impatients ...')
-print('Do not expect other than what it is. Any issues ...just send pm to gedizgursu@gmail\n')
-print('Start Time:',time.ctime(),'\n')
 t1=time.time()
-
+print('Start Time:',time.ctime())
+print('*******************************************************************************************')
+print('A Fast Benchmark designed for impatients by impatients ...')
+print('*******************************************************************************************')
 
 def getcpuinfo(key):
-    with Popen('lscpu', stdout=PIPE) as proc:
-	    out=proc.stdout.read().decode('ascii').split('\n');
-    for i in out:
-        if key in i:
-            return i.replace(key,'')        
+	with Popen('lscpu', stdout=PIPE) as proc:
+		out=proc.stdout.read().decode('ascii').split('\n');
+	for i in out:
+		if key in i:
+			return i.replace(key,'')        
 
 def getgpuinfo():
-    arr=''
-    with Popen('lspci -qq'.split(' '), stdout=PIPE) as proc:
-	    out=proc.stdout.read().decode('ascii').split('\n');
-    for i in out:
-        if 'VGA compatible controller:' in i:
-            s1=i.split('controller: ')[1]
-            dev=i[:7]
-            with Popen(('lspci -ks '+dev).split(' '), stdout=PIPE) as proc:
-	            out=proc.stdout.read().decode('ascii').split('\n');
-            for j in out:
-                if 'Kernel driver in use: ' in j:
-                    s2=j
-                    arr=arr+'GPU: '+s1+'\nSlot: '+dev+'\n'+s2+'\n\n'
-    return arr            
+	arr=''
+	with Popen('lspci -qq'.split(' '), stdout=PIPE) as proc:
+		out=proc.stdout.read().decode('ascii').split('\n');
+	for i in out:
+		if 'VGA compatible controller:' in i:
+			s1=i.split('controller: ')[1]
+			dev=i[:7]
+			with Popen(('lspci -ks '+dev).split(' '), stdout=PIPE) as proc:
+				out=proc.stdout.read().decode('ascii').split('\n');
+			for j in out:
+				if 'Kernel driver in use: ' in j:
+					s2='driver:'+j.split('use:')[1]
+					arr=arr+'GPU: '+s1+' Slot: '+dev+' '+s2
+	return arr            
 
 def testcores(corecount):
-    s=SYSBENCH.replace('THREADS',str(corecount))
-    args=s.split(' ')
-    with Popen(args, stdout=PIPE) as proc:
-	    out=proc.stdout.read().decode('ascii').split('\n');
-    for i in out:
-        if key1 in i:
-            return float(i.replace(key1,'').replace(' ',''))
+	s=SYSBENCH.replace('THREADS',str(corecount))
+	args=s.split(' ')
+	with Popen(args, stdout=PIPE) as proc:
+		out=proc.stdout.read().decode('ascii').split('\n');
+	for i in out:
+		if key1 in i:
+			return float(i.replace(key1,'').replace(' ',''))
 def testmem():
-    args=SYSBENCH2.split(' ')
-    with Popen(args, stdout=PIPE) as proc:
-	    out=proc.stdout.read().decode('ascii').split('\n');
-    for i in out:
-        if 'MiB/sec' in i:
-            return float(i.split('(')[1].split(' MiB')[0])
+	args=SYSBENCH2.split(' ')
+	with Popen(args, stdout=PIPE) as proc:
+		out=proc.stdout.read().decode('ascii').split('\n');
+	for i in out:
+		if 'MiB/sec' in i:
+			return float(i.split('(')[1].split(' MiB')[0])
 
 def gputest():
-    gm2line='glmark2 --fullscreen --off-screen'
-    for i in TESTS:
-        gm2line=gm2line+' -b '+i+':duration=0.25'
-    with Popen(gm2line.split(' '), stdout=PIPE) as proc:
-	    out=proc.stdout.read().decode('ascii').split('\n');
-    for i in out:
-        temp='                                  glmark2 Score:'
-        if temp in i:
-            return float(i.replace(temp,''))
+	gm2line='glmark2 --fullscreen --off-screen'
+	for i in TESTS:
+		gm2line=gm2line+' -b '+i+':duration=0.25'
+	with Popen(gm2line.split(' '), stdout=PIPE) as proc:
+		out=proc.stdout.read().decode('ascii').split('\n');
+	for i in out:
+		temp='                                  glmark2 Score:'
+		if temp in i:
+			return float(i.replace(temp,''))
 
 def whichcard():
-    gm2line='glmark2 --fullscreen --off-screen -b build:duration=0.05'
-    with Popen(gm2line.split(' '), stdout=PIPE) as proc:
-	    out=proc.stdout.read().decode('ascii').split('\n');
-    for i in out:
-        temp='    GL_RENDERER:   '
-        if temp in i:
-            return i.replace(temp,'')
+	gm2line='glmark2 --fullscreen --off-screen -b build:duration=0.05'
+	with Popen(gm2line.split(' '), stdout=PIPE) as proc:
+		out=proc.stdout.read().decode('ascii').split('\n');
+	for i in out:
+		temp='    GL_RENDERER:   '
+		if temp in i:
+			return i.replace(temp,'')
+
+
+
+def listdisks():
+	with Popen("lsblk -l -o NAME,MODEL,TYPE".split(' '), stdout=PIPE) as proc:
+		out=proc.stdout.read().decode('ascii').split('\n')
+
+	venlist=''
+	for i in out:
+		if 'disk' in i:
+			venlist=venlist+i.replace('disk','')
+	print('Disks:', venlist)
 
 def testdisks():
-    with Popen("sudo lshw -class disk".split(' '), stdout=PIPE) as proc:
-	    out=proc.stdout.read().decode('ascii').split('*-disk')[1:4]
-    for j in out:
-        g=j.split('\n')
-        for i in g:
-            temp='vendor: '
-            if temp in i:
-                rs1=i.replace(temp,'')
-            temp='product: '
-            if temp in i:
-                rs2=i.replace(temp,'')
-            temp='logical name: '
-            if temp in i:
-                rs3=i.replace(temp,'')
-        print('Disk:',rs1,rs2,rs3)
+	with Popen("lsblk -l -o NAME,MODEL,TYPE".split(' '), stdout=PIPE) as proc:
+		out=proc.stdout.read().decode('ascii').split('\n')
 
-    with Popen("lsblk -l".split(' '), stdout=PIPE) as proc:
-	    out=proc.stdout.read().decode('ascii').split('\n')
-    
-    for i in out:
-        temp='part '
-        if temp in i:
-            target=i[:7].replace(' ','')
-            with Popen(("sudo hdparm -tT /dev/"+target).split(' '), stdout=PIPE) as proc:
-	            out2=proc.stdout.read().decode('ascii').split('\n')
-            for j in out2:
-                temp='/dev'
-                if temp in j:
-                    r1=j
-                temp='disk read'
-                if temp in j:
-                    r2=j.split('seconds =')[1]
-            print(r1,r2)
-            
-            
+	for i in out:
+		if 'part' in i:
+			target=i.split()[0]
+			with Popen(("sudo hdparm -tT /dev/"+target).split(' '), stdout=PIPE) as proc:
+				out2=proc.stdout.read().decode('ascii').split('\n')
+			for j in out2:
+				temp='/dev'
+				if temp in j:
+					r1=j
+				temp='disk read'
+				if temp in j:
+					r2=j.split('seconds =')[1]
+			print(r1,r2)
+			
+			
 
 
 kpmsc=36.432
@@ -120,25 +115,23 @@ kpmgt=2.544
 kubgt=0.019
 
 kmibsgbs=0.001048576 #MiB/s to GB/s
-print('Cpu:',getcpuinfo('Model name:          '))
-print('Cpu count:',getcpuinfo('CPU(s):              '))
+print('Cpu:',getcpuinfo('Model name:          '),'/',getcpuinfo('CPU(s):              '),'Threads :')
+print(getgpuinfo())
+listdisks()
+print('*******************************************************************************************')
 sc=testcores(1)
 print('SingleCore:',sc ,'   passmark eq:',round(sc*kpmsc),'   userbenchmark eq:',round(sc*kubsc))
 mc=testcores(128)
 print('MultiCore:',mc,'   passmark eq:',round(mc*kpmmc),'   userbenchmark eq:',round(mc*kubmc))
-print(' ')
 ms=testmem()
-print('Memory Speed:',ms*kmibsgbs,'GB/s')
-print(' ')
-print(getgpuinfo())
-print('Rendering On:',whichcard())
+print('\nMemory Speed:',ms*kmibsgbs,'GB/s\n')
+#print('Rendering On:',whichcard())
 gt=gputest()
-print('GPU Score:',gt,'  passmark eq:',round(gt*kpmgt),'  userbenchmark eq:',round(gt*kubgt))
-print('\n')
+print('GPU Score:',gt,'  passmark eq:',round(gt*kpmgt),'  userbenchmark eq:',round(gt*kubgt),'\n')
 testdisks()
-print('\n')
-print('For Extended Octane and V-Ray Benchmarks please add a switch as render-engine ieg:')
-print('"$python3 fastbench.py render-engine"\n')
+print('*******************************************************************************************')
+#print('For Extended Octane and V-Ray Benchmarks please add a switch as render-engine ieg:')
+#print('"$python3 fastbench.py render-engine"\n')
 print('End Time:',time.ctime())
 t2=time.time()
 print('Total Duration (sec):',t2-t1)
